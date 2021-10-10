b4a154eb0f9ded7df636651afa0e4d0774f6524b
ra-fne <82918459+ra-fne@users.noreply.github.com>
2021-10-10 15:50:12 +1100

    latest update

--- js/index.js
+++ js/index.js
@@ -1,2 +1,16 @@
 
-console.log("your index-js file is loaded correctly");
+console.log("done");
+
+/* When the user hovers over"Work", 
+toggle between hiding and showing the dropdown content */
+
+$(".nav ul li").on("hover", function(){
+    $(this).find("#toggle").slideToggle(400); 
+});
+  
+  // Close the dropdown menu if the user clicks outside of it
+ 
+  $(".nav ul li").on("mouseleave", function(){
+    $(this).find("#toggle").slideUp(400); 
+});
+  
