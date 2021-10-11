17fa1d051bd097a8bfc2079b2b0658d222ac2cee
ra-fne <82918459+ra-fne@users.noreply.github.com>
2021-10-11 15:30:06 +1100

    as of Oct 11

--- js/index.js
+++ js/index.js
@@ -1,16 +1,12 @@
 
 console.log("done");
 
-/* When the user hovers over"Work", 
+/* When the user hover over the nav, 
 toggle between hiding and showing the dropdown content */
 
-$(".nav ul li").on("hover", function(){
-    $(this).find("#toggle").slideToggle(400); 
-});
-  
-  // Close the dropdown menu if the user clicks outside of it
+
+  $(function() {
+    $("#work").on("hover", function() {
+        $("#dropDownMenu").slideToggle();
+    });
  
-  $(".nav ul li").on("mouseleave", function(){
-    $(this).find("#toggle").slideUp(400); 
-});
-  
